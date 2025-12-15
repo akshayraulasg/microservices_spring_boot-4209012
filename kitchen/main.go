15c07708c31292db5d5a38db099ad1932d72fab1
CC-Student <akshayraul07@gmail.com>
2025-12-15 05:38:29 +0100

    commit kitchen Microservice

--- kitchen/main.go
+++ kitchen/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"io"
@@ -8,11 +9,27 @@ import (
 	"log/slog"
 	"net/http"
 	"os"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/kelseyhightower/envconfig"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/sethvargo/go-retry"
 )
+
+type Config struct {
+	RabbitMQHost string `default:"localhost"`
+}
 
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 	slog.SetDefault(logger)
+
+	var config Config
+	err := envconfig.Process("kitchen", &config)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
 	r := chi.NewRouter()
 
@@ -20,16 +37,62 @@ func main() {
 		fmt.Fprintf(w, "Hello Kitchen")
 	})
 
-	r.HandleFunc("POST /api/kitchen", func(w http.ResponseWriter, r *http.Request) {
-		bodyBytes, _ := io.ReadAll(r.Body)
-		requestBody := string(bodyBytes)
-		slog.Info("Got it, we're on it ... %v", "body", requestBody)
-		fmt.Fprintf(w, "Got it, we're on it ...")
+	rabbitMqConn := fmt.Sprintf("amqp://guest:guest@%s:5672/", config.RabbitMQHost)
+	slog.Info("connecting to rabbit", "rabbitMqConn", rabbitMqConn)
+
+	retrier := retry.NewFibonacci(3 * time.Second)
+	retrier = retry.WithMaxDuration(60*time.Second, retrier)
+
+	var conn *amqp.Connection
+	var msgs <-chan amqp.Delivery
+	err = retry.Do(context.Background(), retrier, func(ctx context.Context) error {
+		conn, err = amqp.Dial(rabbitMqConn)
+		if err != nil {
+			return retry.RetryableError(err)
+		}
+		ch, err := conn.Channel()
+		if err != nil {
+			return retry.RetryableError(err)
+		}
+
+		slog.Info("Looking for queue kitchen.orders")
+
+		msgs, err = ch.Consume(
+			"kitchen.orders", // queue
+			"",               // consumer
+			true,             // auto-ack
+			false,            // exclusive
+			false,            // no-local
+			false,            // no-wait
+			nil,              // args
+		)
+
+		slog.Info("Not connected", err)
+
+		if err != nil {
+			return retry.RetryableError(err)
+		}
+
+		slog.Info("Succesfully connected to queue kitchen.orders")
+
+		return nil
 	})
+	if err != nil {
+		slog.Info("err connecting", "message", err)
+		log.Fatal(err)
+	}
+
+	defer conn.Close()
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
 
 	port := "8070"
 	slog.Info(fmt.Sprintf("Service kitchen cooking away on port %v ...", port))
-	err := http.ListenAndServe(fmt.Sprintf(":%v", port), r)
+	err = http.ListenAndServe(fmt.Sprintf(":%v", port), r)
 	if err != nil {
 		log.Fatal(err)
 	}
