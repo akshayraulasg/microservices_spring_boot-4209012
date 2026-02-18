983d18e00f9591dfc4fd550794e3d8bdca4d2c57
CC-Student <akshayraul07@gmail.com>
2026-02-18 01:36:07 +0100

    Kafka delivery Microservice

--- delivery/src/main/java/bbq/delivery/config/KafkaConfiguration.java
+++ delivery/src/main/java/bbq/delivery/config/KafkaConfiguration.java
@@ -1,5 +1,19 @@
 package bbq.delivery.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.kafka.config.TopicBuilder;
+
+
+@Component
+@Configuration
 public class KafkaConfiguration {
+
+    @Bean
+    public NewTopic ordersTopic() {
+        return TopicBuilder.name("delivery_updates").build();
+    }
     
 }
